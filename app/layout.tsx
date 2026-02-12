57e232a40f85f3e9fe9c52e38b91442c9fe9920f
Hogwarts26 <dieu0823@gmail.com>
2026-02-12 14:30:49 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ko">
+      <head>
+        {/* 디자인 엔진인 Tailwind를 불러옵니다 */}
+        <script src="https://cdn.tailwindcss.com"></script>
+      </head>
       <body>{children}</body>
     </html>
   )
